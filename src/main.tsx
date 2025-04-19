a7f8275f2341cac08eb67694cb73557d31c9dcaa
codev7777 <james.s200049@gmail.com>
2025-04-19 09:11:53 +0200

    Remove lottie files

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import React from "react";
-import "@lottiefiles/lottie-player";
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
